239500b16f2d234b3db36742bcec1d414ab57869
sri vatsav <srivatsav809@gmail.com>
2016-09-22 10:39:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 app.get('/article-one', function(req, res){
-   res.send("Article 1 requested"); 
+   res.sendFile(path.join(__dirname, 'ui', 'article1.html'));
 });
 
 app.get('/article-two', function(req, res){
