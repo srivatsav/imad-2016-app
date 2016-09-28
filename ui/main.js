aebebaa840ba137aaf2ef86ec5ed209eb4325835
sri vatsav <srivatsav809@gmail.com>
2016-09-28 10:37:39 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,6 +4,6 @@ var counter = 0;
 button.onclick = function clickMeCounter(argument) {
 	// body...
 	counter = counter+1;
-	var span = document.getElementById('count');
+	var span = document.getElementById('num');
 	span.innerHTML = counter.toString();
 };
