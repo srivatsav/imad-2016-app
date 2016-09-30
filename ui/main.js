ae184fe1405d76bc2d0cd5395395cb37bf9c1a1d
sri vatsav <srivatsav809@gmail.com>
2016-09-30 22:09:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -25,15 +25,36 @@ button.onclick = function () {
 
 var nameInput = document.getElementById('name');
 var name = nameInput.value;
-var submit = document.getElementById('submit_btn');
+var submit = document.getElementById('submit_btn'); 
 submit.onclick = function(){
-    var names = ['name1','name2','name3'];
-    var list = '';
-    for(var i=0;i<names.length;i++)
-    {
-        list += '<li>'+names[i]+'</li>';
-    }
     
-var ul = document.getElementById('nameList');
-ul.innerHTML = list;
+    
+	// body...
+	var request = new XMLHttpRequest();
+	
+	request.onreadystatechange = function (){
+	  if(request.readyState === XMLHttpRequest.DONE)
+	  {
+	      if(request.status === 200)
+	      {
+	           var names =  request.responseText;
+	           names = JSON.parse(names);
+                 var list = '';
+                for(var i=0;i<names.length;i++)
+                {
+                    list += '<li>'+names[i]+'</li>';
+                }
+    
+                var ul = document.getElementById('nameList');
+                ul.innerHTML = list;
+	      }
+	  }
+	};
+	
+	request.open('GET', 'http://srivatsav.imad.hasura-app.io/submit-name?name='+name,true);
+	request.send(null);
+	
+	
+
+  
 };
