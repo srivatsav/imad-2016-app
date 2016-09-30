0833e658f38ce3a8147177ecaeab7a9a311aca15
sri vatsav <srivatsav809@gmail.com>
2016-09-30 21:27:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,22 @@ var button = document.getElementById('counter');
 var counter = 0;
 button.onclick = function clickMeCounter(argument) {
 	// body...
-	counter = counter+1;
-	var span = document.getElementById('num');
-	span.innerHTML = counter.toString();
+	var request = new XMLHttpRequest();
+	
+	request.onreadystatechange = function (){
+	  if(request.readyState === XMLHttpRequest.DONE)
+	  {
+	      if(request.status === 200)
+	      {
+	          var counter = request.resopnseText;
+	          var span = document.getElementById('num');
+	          span.innerHTML = counter.toString();
+	      }
+	  }
+	};
+	
+	request.open('GET', 'http://srivatsav.imad.hasura-app.io/counter',true);
+	request.send(null);
+	
+	
 };
