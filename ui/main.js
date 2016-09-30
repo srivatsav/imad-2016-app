741d758849a8f1b9524b2e5cec15570b799147d9
sri vatsav <srivatsav809@gmail.com>
2016-09-30 21:51:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,7 +24,7 @@ button.onclick = function () {
 };
 
 var nameInput = document.getElementById('name');
-var name = nameInput.value
+var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function(){
     var names = ['name1','name2','name3'];
