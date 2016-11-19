ae2156b70d40ea27c50ec514e2bbd82ca5d93736
sri vatsav <srivatsav809@gmail.com>
2016-11-19 11:48:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -142,9 +142,29 @@ app.get('/submit-name',function(req, res) {
     res.send(JSON.stringify(names));
 });
 
-app.get('/:aName', function(req, res){
-    var articleName = req.params.aName;
-   res.send(buildTemplate(articles[articleName]));
+app.get('/articles/:aName', function(req, res){
+    
+    
+    pool.query("select * from article where title = "+req.params.aName,function(err,result)
+    {
+        if(err)
+        {
+            res.status(500).send(err.toString());
+        }
+        else
+        {
+            if(result.rows.length === 0)
+            {
+                res.status(404).send('Article not found');
+            }
+            else
+            {
+                var articleData = result.rows[0];
+                res.send(buildTemplate(articleData));
+            }
+        }
+    });
+   
 });
 app.get('/ui/main.js', function (req, res) {
    res.sendFile (path.join(__dirname, 'ui', 'main.js'));
