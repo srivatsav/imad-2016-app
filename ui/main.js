dd621160021d8868f3d9b66acf7e402b12aa312a
sri vatsav <srivatsav809@gmail.com>
2016-11-19 16:53:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -59,3 +59,38 @@ submit.onclick = function(){
   
 };
 
+var login = document.getElementById('login_btn'); 
+login.onclick = function(){
+    
+	// body...
+	var request = new XMLHttpRequest();
+	
+	request.onreadystatechange = function (){
+	  if(request.readyState === XMLHttpRequest.DONE)
+	  {
+	      if(request.status === 200)
+	      {
+	           alert('Logged in successfully..!!');
+	      }
+	      else if(request.status === 403)
+	      {
+	          alert('Username/password is incorrect');
+	      }
+	      else if(request.status === 500)
+	      {
+	          alert('server error');
+	      }
+	  }
+	};
+	var username = document.getElementById('username').value;
+	var password = document.getElementById('password').value;
+    console.log(username);
+    console.log(password);
+	request.open('POST', 'http://srivatsav.imad.hasura-app.io/login',true);
+	request.setRequestHeader('Content-Type','application/json');
+	request.send(JSON.stringify({username: username,password: password}));
+	
+	
+
+  
+};
