7c75787b8daaa876bc1bea2e91ecfe8945be4660
sri vatsav <srivatsav809@gmail.com>
2016-11-19 18:13:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -214,7 +214,7 @@ app.get('/articles',function(req, res){
            }
            else
            {
-              res.send(JSON.stringify(result.rows));
+              res.send(result.rows);
            }
        }
    });
