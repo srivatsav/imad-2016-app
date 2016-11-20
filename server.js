c63d3f51b18449fcbdefa142b8010d3a6f6312a9
sri vatsav <srivatsav809@gmail.com>
2016-11-20 23:45:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,9 @@ app.use(morgan('combined'));
 app.use(bodyParser.json());
 app.use(session({
     secret: 'randomSecretValue',
-    cookie: {maxAge: 1000*60*60*24*30}
+    cookie: {maxAge: 1000*60*60*24*30},
+    resave: true,
+    saveUninitialized: true
 }));
 var crypto = require('crypto');
 var pool = new Pool(config);
