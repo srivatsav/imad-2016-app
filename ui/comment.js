6d88b2cd90f2e85a10d8009209e0c291d9714a37
sri vatsav <srivatsav809@gmail.com>
2016-11-20 17:40:31 +0530

    Update comment.js

--- ui/comment.js
+++ ui/comment.js
@@ -4,25 +4,9 @@ commentButton.onclick(function(){
 
 });
 */
-var post_id,post_date,post_content;
-var post;
 
-function commentHandler()
+function commentHandler(var id)
 {
-	alert("commentButton clicked..!");
-
-	 post_id = document.getElementById("post_id").innerHTML;
-	 post_date = document.getElementById("post_date").innerHTML;
-	 post_content = document.getElementById("post_content").innerHTML;
-
-	 post =` <hr/>
-		<h3>${post_id}</h3>
-		<div>${post_date}</div>
-		<div>
-			${post_content}
-		</div>
-	</br>
-	<hr/>`;
-	window.location.href="/article.html"
+	alert("commentButton "+id+" clicked..!");	 
 }
 
