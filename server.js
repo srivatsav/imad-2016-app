7198bfe85e403c4a44c4cfb50abeac043d47f909
sri vatsav <srivatsav809@gmail.com>
2016-11-20 23:32:50 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
 var bodyParser = require('body-parser');
+var session = require('express-session');
 var config = {
     user: 'srivatsav',
     databse: 'srivatsav',
@@ -13,6 +14,10 @@ var config = {
 var app = express();
 app.use(morgan('combined'));
 app.use(bodyParser.json());
+app.use(session({
+    secret: 'randomSecretValue',
+    cookie: {maxAge: 1000*60*60*24*30}
+}));
 var crypto = require('crypto');
 var pool = new Pool(config);
 function buildTemplate(data)
@@ -92,9 +97,12 @@ app.post('/login',function(req, res){
               var hashedPassword = hash(password,salt);
               if(hashedPassword===dbString)
               {
+                //set a session
+                req.session.auth = {userId: result.rows[0].id};
+                //set cookie with sessinId
                 res.send('valid credentials.!');    
                 
-                //set a session
+                
               }
               else
               {
@@ -107,7 +115,19 @@ app.post('/login',function(req, res){
 });
 
 
+app.get('/check-login',function(req, res){
+   if(req.session && req.session.auth && req.session.auth.userId){
+       res.send('You are logged in as: '+ req.session.auth.userId.toString());
+   }else{
+       res.send('You are not logged in.');
+   }
+    
+});
 
+app.get('/logout',function(req, res){
+   delete req.session.auth;
+   res.send('You are logged out.');
+});
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
