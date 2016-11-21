861c1d68081fd5fd8257193d513d4e74b999b4dd
sri vatsav <srivatsav809@gmail.com>
2016-11-21 12:29:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -119,7 +119,7 @@ app.post('/login',function(req, res){
 
 app.get('/check-login',function(req, res){
    if(req.session && req.session.auth && req.session.auth.userId){
-       res.send(req.session.auth.userId);
+       res.send(req.session.auth.userId.toString());
    }else{
        res.send('false');
    }
