b43e97af9f8eae94d6f70f380a22e6894b650f66
sri vatsav <srivatsav809@gmail.com>
2016-11-21 13:08:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -140,6 +140,10 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/ui/tabContent.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'tabContent.js'));
+});
+
 //using crypto library foor hashing..
 function hash(input,salt)
 {
