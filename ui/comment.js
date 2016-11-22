12c412a2003668ad824dcf1f3676eea054677d92
sri vatsav <srivatsav809@gmail.com>
2016-11-23 01:16:45 +0530

    Update comment.js

--- ui/comment.js
+++ ui/comment.js
@@ -6,32 +6,7 @@ commentButton.onclick(function(){
 */
 
 function commentHandler(id)
-{
-	var request = new XMLHttpRequest();
-	
-	request.onreadystatechange = function (){
-	  if(request.readyState === XMLHttpRequest.DONE)
-	  {
-	      if(request.status === 200)
-	      {
-		      var respText = request.responseText;
-		      if(respText!='false')
-		      {
-			     
-			       window.location.href = '/post/'+id; 
-		      }
-		      else
-		      {
-			      alert("Please Login to comment.");
-		      }		     
-	           
-              
-	      }
-	  }
-	};
-	
-	request.open('GET', 'http://srivatsav.imad.hasura-app.io/check-login',true);
-	request.send(null);
+{ window.location.href = '/post/'+id; 
 	
 }
 
