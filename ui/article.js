7bf53eef4bafcdd072b19a047a43da5df4016db0
sri vatsav <srivatsav809@gmail.com>
2016-11-23 00:30:29 +0530

    Update article.js

--- ui/article.js
+++ ui/article.js
@@ -1,15 +1,10 @@
-function loadCommentForm () {
-    var commentFormHtml = `
-        <h5>Submit a comment</h5>
-        <textarea id="comment_text" rows="5" cols="100" placeholder="Enter your comment here..."></textarea>
-        <br/>
-        <input type="submit" id="submit" value="Submit" />
-        <br/>
-        `;
-    document.getElementById('comment_form').innerHTML = commentFormHtml;
-    
+var x = window.location.href.split('/')
+          		var currentArticleTitle =x[x.length-1];  
+
+function loadCommentForm () {  
+        
     // Submit username/password to login
-    var submit = document.getElementById('submit');
+    var submit = document.getElementById('submit_comment');
     submit.onclick = function () {
         // Create a request object
         var request = new XMLHttpRequest();
@@ -20,21 +15,77 @@ function loadCommentForm () {
                 // Take some action
                 if (request.status === 200) {
                     // clear the form & reload all the comments
-                    document.getElementById('comment_text').value = '';
+                    document.getElementById('comentArea').value = '';
                     loadComments();    
                 } else {
                     alert('Error! Could not submit comment');
                 }
-                submit.value = 'Submit';
+                submit.value = 'Comment';
           }
         };
         
         // Make the request
-        var comment = document.getElementById('comment_text').value;
+        var comment = document.getElementById('comentArea').value;
         request.open('POST', '/submit-comment/' + currentArticleTitle, true);
         request.setRequestHeader('Content-Type', 'application/json');
         request.send(JSON.stringify({comment: comment}));  
         submit.value = 'Submitting...';
         
     };
+}
+
+
+function loadComments () {
+        // Check if the user is already logged in
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            var comments = document.getElementById('comments');
+            if (request.status === 200) {
+                var content = `	<div class="container" >
+                                    <div class="row">
+                                        <div class="col-sm-8">
+                                            <div class="panel panel-white post panel-shadow">
+                                                <div class="post-heading">
+                                                    <div class="pull-left image">
+                                                        <img src="http://bootdey.com/img/Content/user_1.jpg" class="img-circle avatar" alt="user profile image">
+                                                    </div>
+                                                    <div class="pull-left meta">
+                                                        <div class="title h5">`;
+                var commentsData = JSON.parse(this.responseText);
+                for (var i=0; i< commentsData.length; i++) {
+                    var time = new Date(commentsData[i].timestamp);
+                    
+                        content += `<a href="#"><b>${commentsData[i].username}</b></a>
+                            made a post. </div>
+                        <h6 class="text-muted time">${time.toLocaleTimeString()} on ${time.toLocaleDateString()}</h6>
+                    </div>
+                </div>  
+                <div class="post-description"> 
+                    <p>${commentsData[i].comment}</p>
+                    <div class="stats">
+                        <a href="#" class="btn btn-default stat-item">
+                            <i class="fa fa-thumbs-up icon"></i>2
+                        </a>
+                        <a href="#" class="btn btn-default stat-item">
+                            <i class="fa fa-thumbs-down icon"></i>12
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        </div>
+        
+    </div>`;        
+                }
+                comments.innerHTML = content;
+            } else {
+                comments.innerHTML('Oops! Could not load comments!');
+            }
+        }
+    };
+    
+    request.open('GET', '/get-comments/' + currentArticleTitle, true);
+    request.send(null);
 }
